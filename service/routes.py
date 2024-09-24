ea653fc7c30a786b5aa8ea5327a81349641b1092
coder-monroy <monroyasahel1@gmail.com>
2024-09-23 20:16:22 -0400

    implement read an account

--- service/routes.py
+++ service/routes.py
@@ -59,7 +59,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 @app.route("/accounts", methods=["GET"])
-def get_accounts():
+def list_accounts():
     """
     Lists all Accounts
     This endpoint will list all Accounts
@@ -75,8 +75,19 @@ def get_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
 
-# ... place you code here to READ an account ...
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
